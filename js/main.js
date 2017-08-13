c43371f27ee39ba78efbfc9959cb81518a02ab16
Knut Olav <tyfonknut@gamil.com>
2017-08-13 12:25:24 +0200

    All articles are animated in, checked for input error and made a random button.

--- js/main.js
+++ js/main.js
@@ -1,34 +1,45 @@
-function articleTemplate(title, url, snippet){
-    return '<a target="_blank" href="'+url+ '"><article><h4 class="title">'
-            + title +'</h4><p class="text">'
-            + snippet + '</p></article></a>';
-};
+const SEARCH_RES = $('.searchResult');
 
-$('document').ready(function(){
-  $('#enter').click(function(){
+$('#enter').click(function(){
     wikiData();
-  });
 });
 
 
 
+
 function addWiki(searchWord,titleArray, snippetArray, urlArray){
+
+  SEARCH_RES.hide();
+
   for(let i = 0, n=titleArray.length; i < n; i++){
-      $('.searchResult').append(articleTemplate(titleArray[i],urlArray[i],snippetArray[i]));
+      SEARCH_RES.append(articleTemplate(titleArray[i],urlArray[i],snippetArray[i]));
   }
+  SEARCH_RES.slideDown();
+};
+
+function articleTemplate(title, url, snippet){
+    return '<a target="_blank" href="'+url+ '"><article class="slideIn"><h4 class="title">'
+            + title +'</h4><p class="text">'
+            + snippet + '</p></article></a>';
 };
 
 
 function wikiData(){
-
-var wikiURL = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + $('input[name="searchbox"]').val() + '&format=json&callback=?';
+var searchBox = $('input[name="searchbox"]').val();
+if(searchBox.length == 0){
+  SEARCH_RES.html('<div class="error">The search field is empty.</div>');
+}
+else{
+var wikiURL = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' +
+              searchBox + '&format=json&callback=?';
 
 $.ajax({
     url: wikiURL,
     dataType: "jsonp",
     success: function(data){
-      //console.log(JSON.stringify(data));
         addWiki(...data);
     }
   });
+  }
+  return false;
 };
